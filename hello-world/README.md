b8af45af9c8172660a6cabbccb23f533a030b94b
Bryan Berry <bryan.berry@slalom.com>
2018-08-20 13:35:22 -0700

    hello_world: Add -a/-s OPT parse to api activation
    Usage: ./activate_api.sh\ OPT\
    Required OPT\:
        -i Credentials File [path to]
        -a Activate HTML by populating with API_KEY <OR>
        -s Sanitize HTML by redacting API_KEY

--- hello-world/README.md
+++ hello-world/README.md
@@ -5,6 +5,31 @@ Leaflet CSS is loaded automatically.
 
 Your application must contain `<div id="windy"></div>` in a place, where  you want to position Windy map. Use CSS to resize or position Windy `div` as you wish.
 
+## Activate index.html with your api key
+
+Assuming you have an API key stored as JSON like:
+
+```
+[{
+  "api_key": ""
+  ...
+}]
+```
+
+Run [activate_api.sh](./activate_api.sh) with your API key file:
+
+```
+./activate_api.sh -ai ~/path/to/credfile.json
+```
+
+## Sanitize your api key from index.html
+
+Assuming the above mentioned API key JSON file:
+
+```
+./activate_api.sh -si ~/path/to/credfile.json
+```
+
 In your JS code call function `windyInit( options, callback )`, where options object must contain mandatory API key. Other start-up values are optional, but it is highly recommended to put a much as start-up parameters as possible. 
 
 Your `callback` is called whenever Windy API is ready and as a parameter it receives object with Windy API.
--- hello-world/activate_api.sh
+++ hello-world/activate_api.sh
@@ -2,26 +2,106 @@
 
 # This file activates *.html file w/ required api_key(s)
 
-if [ -z $1 ]; then
-  echo "Please provide a credentials file!"
+SEARCH_KEY='\$WINDY_API_KEY'
+
+function usage {
+
+  echo "Usage: $0 OPT"
+  echo "Required OPT:"
+  echo "    -i Credentials File [path to]"
+  echo "    -a Activate HTML by populating with API_KEY <OR>"
+  echo "    -s Sanitize HTML by redacting API_KEY"
   exit 1
+
+}
+
+if [ $# -lt 1 ]; then
+  usage
 fi
 
-CREDENTIALS=$1
-REPLACE_TEXT='\$WINDY_API_KEY'
+function init {
 
-if [ ! -f $CREDENTIALS ]; then
-  echo "Credentials not found: $CREDENTIALS"
-  exit 2
-fi
+  action=$1
+  credfile=$2
 
-API_KEY=$(cat $CREDENTIALS | jq '.[].api_key' | awk -F '"' '{print $2}')
-while read HTML_FILE; do
-  if [ ! -f $HTML_FILE ]; then
-    echo "Failed to parse file: $HTML_FILE"
-    exit 3
-  else 
-    CMD="sed -i '.bk' -e \"s:$REPLACE_TEXT:$API_KEY:g\" $HTML_FILE"
-    eval $CMD
+  api_key=$(parse_credfile $credfile)
+
+  parse_html $action $api_key
+
+}
+
+function parse_credfile {
+
+  credfile=$1
+
+  cred=$(cat $credfile | jq '.[].api_key' | awk -F '"' '{print $2}')
+  if [[ -z "$cred" ]]; then
+    echo "ERROR: parsing: $credfile"
+    usage $0
+  else
+    echo "$cred" 
   fi
-done< <(find . -name *.html)
+
+}
+
+function parse_html {
+
+  action=$1
+  api_key=$2
+
+  case $action in
+    "activate" )
+      search_text=$SEARCH_KEY
+      replace_text=$api_key
+      ;;
+    "sanitize" )
+      search_text=$api_key
+      replace_text=$SEARCH_KEY
+      ;;
+    *)
+      echo "ERROR: $0 encountered a problem"
+      echo "Aborting"
+      ;;
+  esac
+  
+  while read HTML_FILE; do
+    if [ ! -f $HTML_FILE ]; then
+      echo "Failed to parse file: $HTML_FILE"
+      usage $0
+    else 
+      CMD="sed -i '.bk' -e \"s:$search_text:$replace_text:g\" $HTML_FILE"
+      eval $CMD
+    fi
+  done< <(find . -name *.html)
+
+}
+
+## Run Time Starts Here
+while getopts ":hai:s" opt; do
+  case $opt in
+    h )
+      usage
+      ;;
+    a )
+      action="activate"
+      ;;
+    i )
+      credentials=$OPTARG
+      ;;
+    s )
+      action="sanitize"
+      ;;
+    * )
+      usage $0
+      ;;
+  esac
+done
+
+if [ ! -f $credentials ]; then
+  echo "Credentials not found: $credentials"
+  usage $0
+else
+  init $action $credentials
+  #echo $action
+  #echo $credentials
+fi
